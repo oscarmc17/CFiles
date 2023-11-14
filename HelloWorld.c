d96a6eb46e3702a65f37fa398a80a75f2a88b007
Oscar Caicedo <oscarccd17@gmail.com>
2023-11-14 17:09:51 -0600

    variables, declaration, initialization, etc

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 
-int main(){
+// int main(){
 
-    printf("First C Code!\n");
-    printf("2nd line of code!\n");
+//     /* Multi
+//         Line
+//         Comments
+//     */
+
+//     // Comment block
+//     printf("First \tC Code!\n");
+//     printf("\"2nd\" line of code!");
+//     return 0;
+// }
+
+int main() {
+
+    int x; // declaration
+    x = 123; // initialization
+    int y = 321; // declaration + initialization
+
+    int age = 21; // integer
+    float gpa = 2.05; // floating point number
+    char grade = 'C'; // single character
+    char name[] = "Bro"; //array of characters
+
     return 0;
-}
+};
